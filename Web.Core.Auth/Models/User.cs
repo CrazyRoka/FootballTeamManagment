ed55b099f9f669bedf168fc195464c73b5264f76
Rostyslav Toch <rokarostuk@gmail.com>
2019-03-25 23:30:16 +0200

    Added Web.Core.Auth

---
+++ Web.Core.Auth/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Web.Core.Auth.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<UserRole> UserRoles { get; set; } = new HashSet<UserRole>();
+    }
+}
---
+++ Web.Core.Auth/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Web.Core.Auth.Models;
+
+namespace Web.Core.Auth.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(u => u.Password)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("PasswordHash");
+        }
+    }
+}
